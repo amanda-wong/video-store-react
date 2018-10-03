2441ffef591a29e824c033d5e490d4805fe6de9c
Amanda <missamandawong@gmail.com>
2018-10-02 22:48:16 -0700

    Add year to movie details

--- src/components/display/index.js
+++ src/components/display/index.js
@@ -6,7 +6,7 @@ class Display extends Component {
     render() {
         console.log("Displayed Data: ", this.props.results)
         
-        const results = this.props.results.map((el,i) => <Item key={i} title={el.title} genre={el.genre} actor={el.actor} />);   
+        const results = this.props.results.map((el,i) => <Item key={i} title={el.title} genre={el.genre} actor={el.actor} year={el.year}/>);   
 
         return (
            <div className="display-container">
--- src/components/item/index.js
+++ src/components/item/index.js
@@ -6,6 +6,7 @@ class Item extends Component {
         return (
             <div className="search-item">
                 {this.props.title ? <div className="title">{this.props.title}</div> : null}
+                {this.props.year ? <div className="actor">{this.props.year}</div> : null}
                 {this.props.genre ? <div className="genre">{this.props.genre}</div> : null}
                 {this.props.actor ? <div className="actor">{this.props.actor}</div> : null}
             </div>
